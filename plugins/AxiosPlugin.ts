b4c2fed96dd9de8c51084fa4f5f01f0c5cd1a0e6
Kola771 <koladeaboudou@gmail.com>
2024-06-14 16:33:32 +0200

    intercepteur axios ajouté

--- plugins/AxiosPlugin.ts
+++ plugins/AxiosPlugin.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError, type AxiosResponse } from "axios";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
     const config = useRuntimeConfig();
@@ -6,9 +6,22 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     axios.defaults.baseURL = `${config.public.apiUrl}/api`;
     axios.defaults.headers.common['Content-Type'] = 'application/json'; // on envoie du json au serveur
     axios.defaults.headers.common['Accept'] = 'application/json'; // on attend du serveur du json 
-    axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'; 
+    axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     axios.defaults.withCredentials = true; // 
     axios.defaults.withXSRFToken = true; // 
+
+    // intercepteur de réponse
+    axios.interceptors.response.use(
+        (res) => res,
+        (error: AxiosError) => {
+            if ([401, 419].includes((error.response as AxiosResponse).status)) {
+                const { logout } = useAuth();
+                logout();
+            } else {
+                return Promise.reject(Error);
+            }
+        }
+    );
 
     // On lance une requête pour avoir les cookies nous permettant d'interagir avec le backend
     await axios.get("/sanctum/csrf-cookie", {
